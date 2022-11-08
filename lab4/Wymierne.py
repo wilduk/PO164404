0b0f454359d03c541d8cf9a482b95b4930326173
wilduk <80290557+wilduk@users.noreply.github.com>
2022-11-08 15:25:30 +0100

    Update Wymierne.py
    dodałem typowanie i teraz można dodać (i odjąć!) do liczby wymiernej liczbę całkowitą :>

--- lab4/Wymierne.py
+++ lab4/Wymierne.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 def nwd(p, q):
     reszta = p % q
     if reszta == 0:
@@ -14,65 +17,93 @@ class Wymierna:
         self.p = p//dzielnik
         self.q = q//dzielnik
 
-    def get_licznik(self):
+    def get_licznik(self) -> int:
         return self.p
 
-    def get_mianownik(self):
+    def get_mianownik(self) -> int:
         return self.q
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.p/self.q)
 
-    def __float__(self):
+    def __float__(self) -> float:
         return self.p/self.q
 
-    def __int__(self):
+    def __int__(self) -> int:
         return self.p//self.q
 
-    def __add__(self, other):
+    def __add__(self, other: Union['Wymierna', int]) -> 'Wymierna':
+        if type(other) == int:
+            return Wymierna(self.p+other*self.q, self.q)
         return Wymierna(self.p*other.q+other.p*self.q, self.q*other.q)
 
-    def __sub__(self, other):
+    def __sub__(self, other: Union['Wymierna', int]) -> 'Wymierna':
+        if type(other) == int:
+            return Wymierna(self.p-other*self.q, self.q)
         return Wymierna(self.p*other.q-other.p*self.q, self.q*other.q)
 
-    def __eq__(self, other):
+    def __eq__(self, other: Union['Wymierna', int]) -> bool:
+        if type(other) == int:
+            if not self.q > 1 and not self.q < 1 and not self.p > other and not self.p < other:
+                return True
+            return False
         liczba1 = self.p/self.q
         liczba2 = other.p/other.q
         if not liczba1 > liczba2 and not liczba1 < liczba2:
             return True
         return False
 
-    def __ne__(self, other):
+    def __ne__(self, other: Union['Wymierna', int]) -> bool:
+        if type(other) == int:
+            if self.q == 1 and self.p == other:
+                return False
+            return True
         if self.p == other.p and self.q == other.q:
             return False
         return True
 
-    def __lt__(self, other):
+    def __lt__(self, other: Union['Wymierna', int]) -> bool:
+        if type(other) == int:
+            if self.p/self.q > other:
+                return False
+            return True
         if self.p/self.q < other.p/other.q:
             return True
         return False
 
-    def __le__(self, other):
+    def __le__(self, other: Union['Wymierna', int]) -> bool:
+        if type(other) == int:
+            if self.p/self.q >= other:
+                return False
+            return True
         if self.p/self.q <= other.p/other.q:
             return True
         return False
 
-    def __gt__(self, other):
+    def __gt__(self, other: Union['Wymierna', int]) -> bool:
+        if type(other) == int:
+            if self.p/self.q < other:
+                return False
+            return True
         if self.p/self.q > other.p/other.q:
             return True
         return False
 
-    def __ge__(self, other):
+    def __ge__(self, other: Union['Wymierna', int]) -> bool:
+        if type(other) == int:
+            if self.p/self.q <= other:
+                return False
+            return True
         if self.p/self.q >= other.p/other.q:
             return True
         return False
 
-    def __mul__(self, other):
+    def __mul__(self: 'Wymierna', other: Union['Wymierna', int]) -> 'Wymierna':
         if type(other) == int:
             return Wymierna(self.p * other, self.q)
         return Wymierna(self.p * other.p, self.q * other.q)
 
-    def __truediv__(self, other):
+    def __truediv__(self: 'Wymierna', other: Union['Wymierna', int]) -> 'Wymierna':
         if type(other) == int:
             return Wymierna(self.p, self.q*other)
         return Wymierna(self.p * other.q, other.p * self.q)
@@ -81,9 +112,13 @@ class Wymierna:
 if __name__ == "__main__":
     jeden = Wymierna(1, 2)
     dwa = Wymierna(2, 4)
+    trzy = Wymierna(2, 1)
     print("%d %d" % (jeden.get_licznik(), jeden.get_mianownik()))
     print("%d %d" % (dwa.get_licznik(), dwa.get_mianownik()))
     print("%0.0f" % (float(jeden + dwa)))
     print("%0.2f %0.2f" % (float(jeden * dwa), float(jeden / dwa)))
     print("%0.2f %0.2f" % (jeden * 2, jeden / 2))
     assert(jeden == dwa)
+    assert(trzy == 2)
+    assert(trzy > 1)
+    assert(trzy < 5)

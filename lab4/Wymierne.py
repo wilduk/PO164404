ee158e4963808b382a01b14aea6349d047419a1f
wilduk <80290557+wilduk@users.noreply.github.com>
2022-11-08 15:35:48 +0100

    Update Wymierne.py

--- lab4/Wymierne.py
+++ lab4/Wymierne.py
@@ -47,6 +47,10 @@ class Wymierna:
             if not self.q > 1 and not self.q < 1 and not self.p > other and not self.p < other:
                 return True
             return False
+        elif type(other) == float: # tego już mi się nie chciało robić bez == :p
+            if self.p/self.q == other:
+                return True
+            return False
         liczba1 = self.p/self.q
         liczba2 = other.p/other.q
         if not liczba1 > liczba2 and not liczba1 < liczba2:
@@ -58,42 +62,46 @@ class Wymierna:
             if self.q == 1 and self.p == other:
                 return False
             return True
+        elif type(other) == float:
+            if self.p/self.q == other:
+                return False
+            return True
         if self.p == other.p and self.q == other.q:
             return False
         return True
 
     def __lt__(self, other: Union['Wymierna', int]) -> bool:
-        if type(other) == int:
-            if self.p/self.q > other:
-                return False
-            return True
+        if type(other) == int or type(other) == float:
+            if self.p/self.q < other:
+                return True
+            return False
         if self.p/self.q < other.p/other.q:
             return True
         return False
 
     def __le__(self, other: Union['Wymierna', int]) -> bool:
-        if type(other) == int:
-            if self.p/self.q >= other:
-                return False
-            return True
+        if type(other) == int or type(other) == float:
+            if self.p/self.q <= other:
+                return True
+            return False
         if self.p/self.q <= other.p/other.q:
             return True
         return False
 
     def __gt__(self, other: Union['Wymierna', int]) -> bool:
-        if type(other) == int:
-            if self.p/self.q < other:
-                return False
-            return True
+        if type(other) == int or type(other) == float:
+            if self.p/self.q > other:
+                return True
+            return False
         if self.p/self.q > other.p/other.q:
             return True
         return False
 
     def __ge__(self, other: Union['Wymierna', int]) -> bool:
-        if type(other) == int:
-            if self.p/self.q <= other:
-                return False
-            return True
+        if type(other) == int or type(other) == float:
+            if self.p/self.q >= other:
+                return True
+            return False
         if self.p/self.q >= other.p/other.q:
             return True
         return False
@@ -114,10 +122,16 @@ if __name__ == "__main__":
     dwa = Wymierna(2, 4)
     trzy = Wymierna(2, 1)
     print("%d %d" % (jeden.get_licznik(), jeden.get_mianownik()))
+    assert(jeden.get_licznik() == 1 and jeden.get_mianownik() == 2)
     print("%d %d" % (dwa.get_licznik(), dwa.get_mianownik()))
+    assert(dwa.get_licznik() == 1 and dwa.get_mianownik() == 2)
     print("%0.0f" % (float(jeden + dwa)))
+    assert(jeden + dwa == 1)
     print("%0.2f %0.2f" % (float(jeden * dwa), float(jeden / dwa)))
+    assert(jeden * dwa == 0.25 and jeden / dwa == 1)
     print("%0.2f %0.2f" % (jeden * 2, jeden / 2))
+    assert(jeden * 2 == 1.0 and jeden / 2 == 0.25)
+    assert(jeden == 0.5)
     assert(jeden == dwa)
     assert(trzy == 2)
     assert(trzy > 1)
